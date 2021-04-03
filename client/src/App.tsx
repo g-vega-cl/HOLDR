dda5d65f5c0f84d0f85b98cedd05a37dc6c821a6
g-vega-cl <g.vega.cl@gmail.com>
2021-04-03 11:55:41 -0600

    Created basic structure of site and form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
-const App = () =>{
-    return(
-        <div>
-            <h1>App</h1>
-        </div>
-    )
-}
+import frog from "./images/frog.png";
+import { Form } from "./components/Form/Form";
+import { Posts } from "./components/Posts/Posts";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit" className={classes.appBar}>
+        <Typography variant="h2" align="center" className={classes.heading}>
+          Review
+        </Typography>
+        <img
+          src={frog}
+          alt="feedback"
+          height="60"
+          className={classes.image}
+        ></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch">
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
---
+++ client/src/components/Posts/Post/Post.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import useStyles from "./styles";
+export const Post = () =>{
+    const classes = useStyles();
+    return(
+        <h1>Post</h1>
+    )
+}
